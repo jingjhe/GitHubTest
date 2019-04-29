f9899ace38ea6693cbce7598ecadbf90291bb8ec
nate_huang <nate_huang@xinwang.com.tw>
2019-04-29 18:25:53 +0800

    Complete delete AA_new1

--- app/src/main/java/com/example/nate_huang/githubtest/ScrollingActivity.kt
+++ app/src/main/java/com/example/nate_huang/githubtest/ScrollingActivity.kt
@@ -19,7 +19,6 @@ class ScrollingActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
-            val intent = Intent(this, AAActivity::class.java)
             startActivity(intent)
         }
         println("fileName=>${getPublicAlbumStorageDir("ablumNate").toString()}")
